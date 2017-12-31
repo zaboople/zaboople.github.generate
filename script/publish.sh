03c1f31fa3959c0f54efdf5ebf14f3a4f2e19b7f
zaboople <troymotte@yahoo.com>
2017-12-30 19:02:35 -0600

    Publish
    Former-commit-id: 1133922ea19e158d64e866a439fce78d77409c23

--- script/publish.sh
+++ script/publish.sh
@@ -1,13 +1,20 @@
 #!/bin/bash -e
 cd $(dirname $0)
 ./build.sh dist
+echo
+echo "Committing generator changes..."
 gip-add-commit-and-push-force "Publish"
 cd ../../zaboople.github.io
 echo
-echo "Current directory "$(pwd)
-echo -n "WARNING: I am about to do an rm -rf. Is that ok??? "
+echo "Current directory: "$(pwd)
+echo -n "WARNING: I am about to do an rm -rf. Is that okay? "
 read answer
 if [[ $answer == y* ]]; then
-  echo rm -rf *
+  rm -rf *
 fi
+cd ../zaboople.github.generate
 
+echo
+echo "Current directory: "$(pwd)
+echo -n "WARNING: I am about to do a huge cp -r. Okay? "
+cp -r dist/generated/* ../zaboople.github.io/
