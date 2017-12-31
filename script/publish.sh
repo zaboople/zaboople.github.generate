64f42b8c5f9467cc0754beb616b3b8e8f96de4d8
zaboople <troymotte@yahoo.com>
2017-12-30 19:05:02 -0600

    Publish
    Former-commit-id: 624fa78ec81afb07ba8a59845fc09d2ec4456b9c

--- script/publish.sh
+++ script/publish.sh
@@ -17,4 +17,12 @@ cd ../zaboople.github.generate
 echo
 echo "Current directory: "$(pwd)
 echo -n "WARNING: I am about to do a huge cp -r. Okay? "
-cp -r dist/generated/* ../zaboople.github.io/
+read answer
+if [[ $answer == y* ]]; then
+  cp -r dist/generated/* ../zaboople.github.io/
+fi
+
+echo
+cd ../zaboople.github.io
+gip-add-commit-and-push-force "Publish"
+echo "Hopefully published site"
