fcd779f3be5646350fbe16229ab10dc6b4cc0f1e
zaboople <troymotte@yahoo.com>
2017-12-30 19:17:00 -0600

    Publish
    Former-commit-id: 21ec2b7bb905d7dcf1abfdb68b97d2602456bd76

--- script/publish.sh
+++ script/publish.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
-cd $(dirname $0)
-./build.sh dist
+cd $(dirname $0)/..
+ant dist
 echo
 echo "Committing generator changes..."
 gip-add-commit-and-push-force "Publish"
 
-cd ../../zaboople.github.io
+cd ../zaboople.github.io
 echo
 echo "Current directory: "$(pwd)
 echo -n "WARNING: I am about to do an rm -rf. Is that okay? "
